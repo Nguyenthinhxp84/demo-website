2d15c3dcef4b102fe983502750fb997a5b1365c0
Nguyenthinhxp84 <90415911+Nguyenthinhxp84@users.noreply.github.com>
2021-09-23 22:11:57 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -34,16 +34,3 @@ menuTitle.addEventListener("click",function(x) {
 //	naviList.classList.toggle('active');
 //})
 
-if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", function () {
-        tilt([event.beta, event.gamma]);
-    }, true);
-} else if (window.DeviceMotionEvent) {
-    window.addEventListener('devicemotion', function () {
-        tilt([event.acceleration.x * 2, event.acceleration.y * 2]);
-    }, true);
-} else {
-    window.addEventListener("MozOrientation", function () {
-        tilt([orientation.x * 50, orientation.y * 50]);
-    }, true);
-}

61c4b71a39dbfda98cac8701806590f6e231314a
Nguyenthinhxp84 <90415911+Nguyenthinhxp84@users.noreply.github.com>
2021-09-26 10:23:58 +0700

    1

--- js/scripts.js
+++ js/scripts.js
@@ -34,24 +34,3 @@ menuTitle.addEventListener("click",function(x) {
 //	naviList.classList.toggle('active');
 //})
 
- window.onscroll = function () {
-        var header_navbar = document.querySelector(".navbar-area");
-        var sticky = header_navbar.offsetTop;
-
-        var logo = document.querySelector('.navbar-brand img')
-        if (window.pageYOffset > sticky) {
-          header_navbar.classList.add("sticky");
-          logo.src = 'assets/images/logo/Logo_IUH.png';
-        } else {
-          header_navbar.classList.remove("sticky");
-          logo.src = 'assets/images/logo/Logo_IUH.png';
-        }
-
-        // show or hide the back-top-top button
-        var backToTo = document.querySelector(".scroll-top");
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            backToTo.style.display = "flex";
-        } else {
-            backToTo.style.display = "none";
-        }
-    };
